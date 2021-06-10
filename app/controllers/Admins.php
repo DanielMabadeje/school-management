07d5c4ab274199baed6928b3398c04be4550d8ea
DanielMabadeje <mabadejedaniel1@gmail.com>
2021-06-10 12:10:43 -0700

    added files

--- app/controllers/Admins.php
+++ app/controllers/Admins.php
@@ -5,20 +5,12 @@
  * in order to load a view inside a folder of the view folder
  * the folder/filename must be parsed
  */
-class Pages extends Controller
+class Admins extends Controller
 {
     public function __construct()
     {
         $this->userModel = $this->model('User');
         $this->studentModel = $this->model('Student');
-    }
-    public function about()
-    {
-        $data = [
-            'title' => 'About',
-            'description' => "App to share posts"
-        ];
-        $this->view('pages/about', $data);
     }
     public function Index()
     {
