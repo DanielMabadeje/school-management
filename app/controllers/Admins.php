5f417e066c4a960cf268ca3f74ef37a81bf2527d
DanielMabadeje <mabadejedaniel1@gmail.com>
2021-06-10 12:14:45 -0700

    editing dashboard

--- app/controllers/Admins.php
+++ app/controllers/Admins.php
@@ -9,10 +9,15 @@ class Admins extends Controller
 {
     public function __construct()
     {
+        if (!isLoggedIn() && $_SESSION['membership_group'] !== "admin") {
+            redirect('/');
+        }
         $this->userModel = $this->model('User');
         $this->studentModel = $this->model('Student');
     }
     public function Index()
     {
+
+        $this->view('admin/index');
     }
 }
---
+++ app/views/admin/index.php
@@ -0,0 +1,1 @@
+<?php require APPROOT . '/views/admin/inc/header.php';
