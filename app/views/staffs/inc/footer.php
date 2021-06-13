d275bb810d19ebe234eec67fd157b96577d4f903
DanielMabadeje <mabadejedaniel1@gmail.com>
2021-06-13 12:21:39 -0700

    edited footer

---
+++ app/views/staffs/inc/footer.php
@@ -0,0 +1,7 @@
+<script src="<?= URLROOT ?>/js/jquery.js"></script>
+<script src="<?= URLROOT ?>/js/dashboard/links.js"></script>
+<script src="<?= URLROOT ?>/js/accordion.js"></script>
+
+</body>
+
+</html>
