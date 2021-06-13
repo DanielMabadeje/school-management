aa2db5c464d5bb63ad9283fe348b8284b24837bd
DanielMabadeje <mabadejedaniel1@gmail.com>
2021-06-13 12:15:21 -0700

    added staffs

---
+++ app/controllers/Staffs.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Every page loads from view folder
+ * in order to load a view inside a folder of the view folder
+ * the folder/filename must be parsed
+ */
+class Staffs extends Controller
+{
+    public function __construct()
+    {
+        if (!isLoggedIn() && $_SESSION['membership_group'] !== "staff") {
+            redirect('/');
+        }
+        $this->userModel = $this->model('User');
+        $this->studentModel = $this->model('Student');
+    }
+}
