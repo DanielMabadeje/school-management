4123db790ef42ed3b54347b61e5e44d991ef6540
DanielMabadeje <mabadejedaniel1@gmail.com>
2021-06-13 12:38:24 -0700

    edited index.php

---
+++ app/views/admin/addExam.php
@@ -0,0 +1,66 @@
+<?php require APPROOT . '/views/admin/inc/header.php'; ?>
+
+
+<div class="container ">
+
+    <div class="col-md-12">
+
+
+        <div class="header-side pt-5 mt-5">
+            <h1 class="display-4">Add Exams</h1>
+
+            <hr>
+        </div>
+
+
+
+
+
+        <div class="col-md-12 ">
+                <div class="card ">
+
+                    <div class="card-header pt-0 pb-0 bg-other-faint">
+                        <h4>Users</h4>
+                    </div>
+                    <div class="card-body p-0">
+
+                        <div class="pt-0 pb-1">
+                            <h5 class="mb-0"></h5>
+                            <p class="small text-muted"></p>
+                            <table class="table col-12">
+                                <thead>
+                                    <tr>
+                                        <th> Name </th>
+                                        <th> Registered </th>
+                                    </tr>
+                                </thead>
+                                <tbody id="order">
+                                    <tr>
+                                        <td>Paypal</td>
+                                        <td> $14,000</td>
+                                    </tr>
+                                    <tr>
+
+                                        <td>Visa</td>
+                                        <td>$41,160</td>
+                                    </tr>
+                                    <tr>
+
+                                        <td>Paypal</td>
+                                        <td>$12,603</td>
+                                    </tr>
+                                    <tr>
+
+                                        <td>Paypal</td>
+                                        <td>$91,231</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+       
+    </div>
+</div>
---
+++ app/views/staffs/index.php
@@ -0,0 +1,78 @@
+<?php require APPROOT . '/views/staffs/inc/header.php'; ?>
+<div class="main-content">
+  <?php require APPROOT . '/views/staffs/inc/navbar.php'; ?>
+  <link rel="stylesheet" href="<?= URLROOT ?>/css/calendar.css">
+
+  <main>
+    <!-- <div class="uid">
+				<h1>Hello, <span>User Full Name</span></h1><br>
+				<p>Time: <?php echo date('d-m-y h:i:s'); ?></p>
+			</div> -->
+
+    <!-- <div>
+				<p>The rest of the content is here.</p>
+			</div> -->
+
+
+    <section class="calendar">
+      <div class="month">
+        <ul>
+          <li class="prev">&#10094;</li>
+          <li class="next">&#10095;</li>
+          <li>
+            August<br>
+            <span style="font-size:18px">2016</span>
+          </li>
+        </ul>
+      </div>
+
+      <ul class="weekdays">
+        <li>Mo</li>
+        <li>Tu</li>
+        <li>We</li>
+        <li>Th</li>
+        <li>Fr</li>
+        <li>Sa</li>
+        <li>Su</li>
+      </ul>
+
+      <ul class="days">
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+        <li>4</li>
+        <li>5</li>
+        <li>6</li>
+        <li>7</li>
+        <li>8</li>
+        <li>9</li>
+        <li><span class="active">10</span></li>
+        <li>11</li>
+        <li>12</li>
+        <li>13</li>
+        <li>14</li>
+        <li>15</li>
+        <li>16</li>
+        <li>17</li>
+        <li>18</li>
+        <li>19</li>
+        <li>20</li>
+        <li>21</li>
+        <li>22</li>
+        <li>23</li>
+        <li>24</li>
+        <li>25</li>
+        <li>26</li>
+        <li>27</li>
+        <li>28</li>
+        <li>29</li>
+        <li>30</li>
+        <li>31</li>
+        <!-- ...etc -->
+      </ul>
+    </section>
+
+  </main>
+</div>
+
+<?php require APPROOT . '/views/staffs/inc/footer.php'; ?>
