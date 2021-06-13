c2715c1e2b73b48eed6bacfca505493761a43b45
DanielMabadeje <mabadejedaniel1@gmail.com>
2021-06-13 12:02:48 -0700

    added query

--- app/models/Student.php
+++ app/models/Student.php
@@ -101,7 +101,13 @@ class Student
                               WHERE students_profile.faculty_id= :faculty_id');
             $this->db->bind(':faculty_id', $faculty_id);
         } else {
-            $this->db->query('SELECT * FROM students_profile WHERE faculty_id= :faculty_id AND level=:level');
+            $this->db->query('SELECT students.user_id, students.name, students.regNo AS reg_no, students_profile.faculty_id, students_profile.department_id, departments.name AS department
+                              FROM students_profile
+                              INNER JOIN students
+                              ON students_profile.user_id=students.user_id
+                              INNER JOIN departments
+                              ON departments.id=students_profile.department_id
+                              WHERE students_profile.faculty_id= :faculty_id AND level=:level');
             $this->db->bind(':faculty_id', $faculty_id);
             $this->db->bind(':level', $level);
         }
