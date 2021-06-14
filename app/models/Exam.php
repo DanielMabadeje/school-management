33bd3447fde28921f7a9c85a72bbbe0e9b51c25b
DanielMabadeje <mabadejedaniel1@gmail.com>
2021-06-14 10:15:08 -0700

    edited exam

---
+++ app/models/Exam.php
@@ -0,0 +1,17 @@
+<?php
+
+// namespace App\Models;
+
+class Exam
+{
+    private $db;
+
+    /**
+     * Post constructor.
+     * @param null $data
+     */
+    public function __construct()
+    {
+        $this->db = new Database;
+    }
+}
