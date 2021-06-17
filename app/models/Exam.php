84146904a226283c866044d7de989b5bf3bb0494
DanielMabadeje <mabadejedaniel1@gmail.com>
2021-06-17 12:31:00 -0700

    edited exam model

--- app/models/Exam.php
+++ app/models/Exam.php
@@ -16,12 +16,19 @@ class Exam
     }
     public function addExam($data)
     {
-        var_dump($data);
+        // var_dump($data);
 
         $this->db->query('INSERT INTO exams (name, description, course_id, department_id) VALUES(:name, :description, :course_id, :department_id)');
         $this->db->bind(':name', $data['name']);
         $this->db->bind(':description', $data['description']);
         $this->db->bind(':course_id', $data['course_id']);
         $this->db->bind(':department_id', $data['department_id']);
+
+
+        if ($this->db->execute()) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
