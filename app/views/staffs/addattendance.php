a64cac4d073d9d1574c69022e3af2386dd4ca8ec
DanielMabadeje <mabadejedaniel1@gmail.com>
2021-06-18 13:45:09 -0700

    added add attendance

---
+++ app/views/staffs/addattendance.php
@@ -0,0 +1,18 @@
+<?php require APPROOT . '/views/staffs/inc/header.php'; ?>
+<div class="main-content">
+    <?php require APPROOT . '/views/staffs/inc/navbar.php'; ?>
+    <link rel="stylesheet" href="<?= URLROOT ?>/css/calendar.css">
+
+    <main>
+        <section>
+            <div class="attendance">
+                <h2 class="display-4">Add Attendance</h2>
+
+
+             
+            </div>
+        </section>
+    </main>
+</div>
+
+<?php require APPROOT . '/views/staffs/inc/footer.php'; ?>
