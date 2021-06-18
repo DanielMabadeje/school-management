f169cb11ba940dcc75ae3ebae2264b26787d8fb1
DanielMabadeje <mabadejedaniel1@gmail.com>
2021-06-18 12:58:58 -0700

    edited attendance

---
+++ app/views/staffs/attendance.php
@@ -0,0 +1,11 @@
+<?php require APPROOT . '/views/staffs/inc/header.php'; ?>
+<div class="main-content">
+    <?php require APPROOT . '/views/staffs/inc/navbar.php'; ?>
+    <link rel="stylesheet" href="<?= URLROOT ?>/css/calendar.css">
+
+    <main>
+
+    </main>
+</div>
+
+<?php require APPROOT . '/views/staffs/inc/footer.php'; ?>
