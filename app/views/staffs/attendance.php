999889ea2b4d1e89a1bd6943ed0724e0c5050153
DanielMabadeje <mabadejedaniel1@gmail.com>
2021-06-18 13:35:27 -0700

    added getstaff by id

--- app/views/staffs/attendance.php
+++ app/views/staffs/attendance.php
@@ -13,6 +13,12 @@
                     <div class="mt-5 pt-5">
                         <div class="card">
                             <div class="card-body">
+
+                            <div class="col-md-12">
+                                <div class="col-md-4 ml-auto text-right">
+                                    <a href="" class="btn btn-primary ml-auto">Add Attendance</a>
+                                </div>
+                            </div>
                                 <div class="card-title mb-0">Students</div>
                                 <div class="table-responsive">
                                     <table class="table text-left">
