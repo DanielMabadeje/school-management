6e31356b30e479ef1bd1f2b31551e9ae64223924
DanielMabadeje <mabadejedaniel1@gmail.com>
2021-06-19 12:01:34 -0700

    Added add attendance

---
+++ app/models/Attendance.php
@@ -0,0 +1,21 @@
+<?php
+
+// namespace App\Models;
+
+class Attendance
+{
+    private $db;
+
+    /**
+     * Post constructor.
+     * @param null $data
+     */
+    public function __construct()
+    {
+        $this->db = new Database;
+    }
+    public function addAttendance(Type $var = null)
+    {
+        # code...
+    }
+}
