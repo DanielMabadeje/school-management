ebc841a20bf12d5c5be4dafe078e4ef2995a968f
DanielMabadeje <mabadejedaniel1@gmail.com>
2021-06-19 12:09:57 -0700

    edited add attendance

--- app/models/Attendance.php
+++ app/models/Attendance.php
@@ -17,8 +17,17 @@ class Attendance
     public function addAttendance($data)
     {
         $this->db->query(
-            "INSERT INTO attendance
-            "
+            "INSERT INTO attendance (name, course_id, week) VALUES(:name, :course_id, :week)"
         );
+
+        $this->db->bind(":name", $data['name']);
+        $this->db->bind(":course_id", $data['course_id']);
+        $this->db->bind(":week", $data['week']);
+
+        if ($this->db->execute()) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
