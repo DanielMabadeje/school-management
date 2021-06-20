821370df278d5a8e178390ffbf453c3c5f402553
DanielMabadeje <mabadejedaniel1@gmail.com>
2021-06-20 09:13:32 -0700

    edited courses

--- app/views/admin/inc/navbar.php
+++ app/views/admin/inc/navbar.php
@@ -42,6 +42,21 @@
                     </div>
                 </li>
 
+
+
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle text-dark" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Courses
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="<?= URLROOT ?>/admins/addCourse">Add Courses</a>
+
+                        <div class="dropdown-divider"></div>
+
+                    </div>
+                </li>
+
+
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle text-dark" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         Exams
