bcc7eb47567dd35c16fe769651e2149bf0b4ed9e
DanielMabadeje <mabadejedaniel1@gmail.com>
2021-06-20 08:39:11 -0700

    edited  course model

---
+++ app/models/Course.php
@@ -0,0 +1,17 @@
+<?php
+
+// namespace App\Models;
+
+class Course
+{
+    private $db;
+
+    /**
+     * Post constructor.
+     * @param null $data
+     */
+    public function __construct()
+    {
+        $this->db = new Database;
+    }
+}
