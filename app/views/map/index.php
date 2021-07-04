d09c6696793c801bc9463b872d58c5c0c6edb332
DanielMabadeje <mabadejedaniel1@gmail.com>
2021-07-04 08:12:41 -0700

    added footer.php

--- app/views/map/index.php
+++ app/views/map/index.php
@@ -1,7 +1,11 @@
 <?php require APPROOT . '/views/map/inc/header.php'; ?>
 
+
 <div class="header"></div>
 
 <div class="main">
     <div id="map"></div>
 </div>
+
+
+<?php require APPROOT . '/views/map/inc/footer.php'; ?>
