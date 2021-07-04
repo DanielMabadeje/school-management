4cbb70fd9fec4925f5134c7d3a8a14e3e8551c48
DanielMabadeje <mabadejedaniel1@gmail.com>
2021-07-04 08:21:41 -0700

    getting location with geolovation api

---
+++ public/js/map/map.js
@@ -0,0 +1,12 @@
+
+
+//get the location ia Geoloion API
+
+if ('geolocation' in  navigator) {
+    var currentLocation = navigator.geolocation.getCurrentPosition(function(){
+        map.setCenter({
+            lat:position.coords.latitude,
+            lng:position.coords.longitude,
+        });
+    });
+}
