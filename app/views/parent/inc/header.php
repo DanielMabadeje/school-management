ff481078673eb372d7ea97ffd06b2ca51cf4f5e8
DanielMabadeje <mabadejedaniel1@gmail.com>
2021-07-04 06:48:38 -0700

    edited header.php

---
+++ app/views/parent/inc/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>RITMAN | Parent Dashboard</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <link rel="stylesheet" type="text/css" href="https://maxst.icons8.com/vue-static/landings/line-awesome/1.3.0/css/line-awesome.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="<?= URLROOT ?>/css/dashboard/dashboard.css">
+    <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/accordion.css">
+
+
+</head>
+
+<body class="bg-light">
+
+    <input type="checkbox" name="" id="nav-toggle">
+    <?php require APPROOT . '/views/parent/inc/sidebar.php'; ?>
