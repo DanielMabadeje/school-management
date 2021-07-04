9aa715479bc46d8c55c986b6590443032b1f21fb
DanielMabadeje <mabadejedaniel1@gmail.com>
2021-07-04 08:31:47 -0700

    added createLocationChannel

--- public/js/map/map.js
+++ public/js/map/map.js
@@ -15,3 +15,26 @@ var pusher = new Pusher('', {
     cluster:'',
     encrypted: true
 })
+
+
+function createMyLocationChannel(name) {
+    var myLocationChannel = pusher.subscribe('private-' + name);
+    myLocationChannel.bind('pusher:subscription_succeeded', function (params) {
+        
+        // safe to now trigger events
+        // use the watchPosition API to watch the changing location
+        // and trigger events with new coordinates
+
+        locationWatcher=navigator.geolocation.watchPosition(function (position) {
+            var location = {
+                lat:position.coords.latitude,
+                lng:position.coords.longitude
+            };
+
+            triggerLocationChangeEvents(myLocationChannel, location);
+            
+        });
+
+        // also start a setInterval to keep sending the location every 5  secs
+    })
+}
