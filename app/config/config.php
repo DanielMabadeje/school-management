32ea0a3224d2d19a29be02418e12116a20469d3e
DanielMabadeje <mabadejedaniel1@gmail.com>
2021-07-05 13:25:17 -0700

    edited config

--- app/config/config.php
+++ app/config/config.php
@@ -6,7 +6,7 @@ define('DB_PASS', '');
 define('DB_NAME', 'schoolmanage');
 //App Root
 define('APPROOT', dirname(dirname(__FILE__)));
-// define('URLROOT', 'http://localhost/schoolmanage/');
-define('URLROOT', 'http://schoolmanage.test/');
+define('URLROOT', 'http://localhost/schoolmanage/');
+// define('URLROOT', 'http://schoolmanage.test/');
 //site name
 define('SITENAME', 'RITMAN UNIVERSITY');
