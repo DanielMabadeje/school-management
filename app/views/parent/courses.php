e3c489660938d635ee70dab478ec68cc06fac5ed
DanielMabadeje <mabadejedaniel1@gmail.com>
2021-07-07 12:51:26 -0700

    Added files

---
+++ app/views/parent/courses.php
@@ -0,0 +1,29 @@
+<?php require APPROOT . '/views/students/inc/header.php'; ?>
+<div class="main-content">
+  <?php require APPROOT . '/views/students/inc/dashnav.php'; ?>
+  <link rel="stylesheet" href="<?= URLROOT ?>/css/calendar.css">
+
+  <main>
+    <!-- <div class="uid">
+				<h1>Hello, <span>User Full Name</span></h1><br>
+				<p>Time: <?php echo date('d-m-y h:i:s'); ?></p>
+			</div> -->
+
+    <!-- <div>
+				<p>The rest of the content is here.</p>
+			</div> -->
+
+    <h2>Courses</h2>
+
+    <div class="bg-white mt-5 pt-5">
+      <?php foreach ($data['courses'] as $course) : ?>
+        <button class="accordion"><?= $course->name ?></button>
+        <div class="panel mb-3">
+          <p><?= $course->description ?></p>
+        </div>
+      <?php endforeach; ?>
+    </div>
+
+  </main>
+</div>
+<?php require APPROOT . '/views/students/inc/footer.php'; ?>
---
+++ app/views/parent/students.php
@@ -0,0 +1,58 @@
+<?php require APPROOT . '/views/students/inc/header.php'; ?>
+<div class="main-content">
+    <?php require APPROOT . '/views/students/inc/dashnav.php'; ?>
+    <link rel="stylesheet" href="<?= URLROOT ?>/css/calendar.css">
+
+    <main>
+        <!-- <div class="uid">
+				<h1>Hello, <span>User Full Name</span></h1><br>
+				<p>Time: <?php echo date('d-m-y h:i:s'); ?></p>
+			</div> -->
+
+        <!-- <div>
+				<p>The rest of the content is here.</p>
+			</div> -->
+
+        <h2>Students</h2>
+
+
+        <div class="mt-5 pt-5">
+            <div class="card">
+                <div class="card-body">
+                    <div class="card-title mb-0">Students</div>
+                    <div class="table-responsive">
+                        <table class="table text-left">
+                            <thead>
+                                <tr class="bg-light text-dark">
+                                    <th> Name </th>
+                                    <th> Reg No </th>
+                                    <th> Status </th>
+                                    <th> Course</th>
+                                    <th> Academic Year</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+
+                                <?php foreach ($data['students'] as $student) : ?>
+                                    <tr>
+                                        <td><?= $student->name ?></td>
+                                        <td><?= $student->reg_no ?></td>
+                                        <td>Student</td>
+                                        <td><?= $student->department ?></td>
+                                        <td><?= date('Y') ?></td>
+                                    </tr>
+
+                                <?php endforeach; ?>
+
+                    </div>
+
+
+                    </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+
+    </main>
+</div>
+<?php require APPROOT . '/views/students/inc/footer.php'; ?>
