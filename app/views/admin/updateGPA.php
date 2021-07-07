219049c280deb852f424263745f6df9852ca0d86
DanielMabadeje <mabadejedaniel1@gmail.com>
2021-07-07 13:35:41 -0700

    edited view

---
+++ app/views/admin/updateGPA.php
@@ -0,0 +1,50 @@
+<?php require APPROOT . '/views/admin/inc/header.php'; ?>
+
+
+<div class="container ">
+
+    <div class="col-md-12">
+
+
+        <div class="header-side pt-5 mt-5">
+            <h1 class="display-4">Add Test</h1>
+
+            <hr>
+        </div>
+
+
+
+
+
+        <div class="col-md-12 ">
+            <div class="card ">
+
+                <div class="card-header pt-0 pb-0 bg-other-faint">
+                    <h4>Add Test</h4>
+                </div>
+                <div class="card-body p-0">
+                    <div class="form p-5">
+                        <form action="" method="post">
+                            <div class="form-group">
+                                <label for="name">GPA <sup>*</sup></label>
+                                <input type="text" name="gpa" class="form-control form-control-lg" value="">
+                                <span class="invalid-feedback"></span>
+                            </div>
+
+
+                            <div class="form-group">
+                                <input type="submit" value="Update GPA" class="btn btn-primary btn-block col-6 p-3">
+                            </div>
+
+
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+    </div>
+</div>
+
+<?php require APPROOT . '/views/admin/inc/footer.php'; ?>
